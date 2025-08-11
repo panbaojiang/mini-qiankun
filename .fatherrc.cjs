15ee0b98f0b3053acc161e7698f9416f09f539b4
潘保江 <mr_jiang@panbaojiangdeMacBook-Pro.local>
2025-08-11 19:13:34 +0800

    packages包构建缺失

--- .fatherrc.cjs
+++ .fatherrc.cjs
@@ -1,16 +1,6 @@
 module.exports = {
-  platform: 'browser',        // 目标平台：浏览器环境
-  esm: {},                    // ES Module 输出配置
-  cjs: {},                    // CommonJS 输出配置  
-  sourcemap: true,            // 生成 sourcemap
-  extraBabelPlugins: [        // 额外的 Babel 插件
-    [
-      'babel-plugin-import',  // lodash 按需导入插件
-      {
-        libraryName: 'lodash',
-        libraryDirectory: '',
-        camel2DashComponentName: false,
-      },
-    ],
-  ],
+  platform: "browser", // 目标平台：浏览器环境
+  esm: {}, // ES Module 输出配置
+  cjs: {}, // CommonJS 输出配置
+  sourcemap: true, // 生成 sourcemap
 };
