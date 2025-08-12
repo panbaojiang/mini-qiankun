5efed58de3a9f5db42fada5e964138d497c03f8c
潘保江 <mr_jiang@panbaojiangdeMacBook-Pro.local>
2025-08-12 15:49:29 +0800

    添加注册域名

--- apps/main-app/src/config/index.ts
+++ apps/main-app/src/config/index.ts
@@ -1,20 +1,22 @@
 // 应用配置
+const { VITE_SUB_VUE2_ENTRY, VITE_SUB_VUE3_ENTRY, VITE_SUB_REACT_ENTRY } =
+  import.meta.env;
 export const config = {
   // 子应用配置
   subApps: {
     vue2: {
       name: 'sub-vue2',
-      entry: '//localhost:7101',
+      entry: VITE_SUB_VUE2_ENTRY,
       activeRule: '/sub-vue2',
     },
     vue3: {
       name: 'sub-vue3',
-      entry: '//localhost:7102',
+      entry: VITE_SUB_VUE3_ENTRY,
       activeRule: '/sub-vue3',
     },
     react: {
       name: 'sub-react',
-      entry: '//localhost:7103',
+      entry: VITE_SUB_REACT_ENTRY,
       activeRule: '/sub-react',
     },
   },
